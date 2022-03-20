b218a84662e48ecb5a1b49760f6204b5dd62b9a1
Securicraft <31308657+Securicraft@users.noreply.github.com>
2022-03-20 15:43:33 +0700

    Add directive symbol

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from Cython.Build import cythonize;
 
 setup(
     name = "ControllerApp",
-    ext_modules = cythonize("Controller.pyx"),
-    zip_safe=False,
+    ext_modules = cythonize("Controller_v2.py", compiler_directives={'language_level' : "3"} ),
+    zip_safe    = False,
+    
     );
